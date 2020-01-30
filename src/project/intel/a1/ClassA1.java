f3bf558e4d73ebb97f40eed8c96f9b7e38fa1acc
BengirMeera <miroremage.6667@gmail.com>
2020-01-29 22:19:54 -0500

    My first commit

---
+++ src/project/intel/a1/ClassA1.java
@@ -0,0 +1,19 @@
+package project.intel.a1;
+
+public class ClassA1 {
+int height=53;
+double size=3.5;
+boolean Sylinder= true;
+
+
+     public static void main(String []args){
+         ClassA1 classA1 =new ClassA1();
+        System.out.println("This is ok");
+    }
+
+
+
+
+
+}
+
